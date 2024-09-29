c558e1dd3e940a0412a42ae2c548c948ffb462b5
vedasole <vedasole001@gmail.com>
2024-09-30 01:54:38 +0530

    feat(country): add CountryController for handling web requests

---
+++ src/main/java/com/vedasole/flight_service/contoller/CountryController.java
@@ -0,0 +1,56 @@
+package com.vedasole.flight_service.contoller;
+
+import com.vedasole.flight_service.payload.CountryDto;
+import com.vedasole.flight_service.service.service_interface.CountryService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/countries")
+@AllArgsConstructor
+public class CountryController {
+
+    private final CountryService countryService;
+
+    @PostMapping
+    public ResponseEntity<CountryDto> addCountry(@Valid CountryDto countryDto) {
+        CountryDto savedCountryDto = countryService.addCountry(countryDto);
+        return new ResponseEntity<>(savedCountryDto, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{countryId}")
+    public ResponseEntity<CountryDto> updateCountry(
+            @PathVariable String countryId,
+            @Valid CountryDto countryDto
+    ) {
+        CountryDto updatedCountryDto = countryService.updateCountry(countryId, countryDto);
+        return new ResponseEntity<>(updatedCountryDto, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{countryId}")
+    public ResponseEntity<CountryDto> deleteCountry(@PathVariable String countryId) {
+        CountryDto deletedCountryDto = countryService.deleteCountry(countryId);
+        return new ResponseEntity<>(deletedCountryDto, HttpStatus.OK);
+    }
+
+    @GetMapping("/{countryId}")
+    public ResponseEntity<CountryDto> getCountryById(@PathVariable String countryId) {
+        CountryDto countryDto = countryService.getCountryById(countryId);
+        return new ResponseEntity<>(countryDto, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<CountryDto>> getAllCountries(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "countryId") String sortBy,
+            @RequestParam(defaultValue = "asc") String order
+    ) {
+        return new ResponseEntity<>(countryService.getAllCountries(page, size, sortBy, order), HttpStatus.OK);
+    }
+
+}
