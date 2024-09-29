05ddad4b1ed4c85cb07fcb40a9a4df1b56b32e74
vedasole <vedasole001@gmail.com>
2024-09-30 01:49:17 +0530

    feat(country): add CountryDto class and CountryRepo interface

---
+++ src/main/java/com/vedasole/flight_service/payload/CountryDto.java
@@ -0,0 +1,51 @@
+package com.vedasole.flight_service.payload;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CountryDto {
+
+    private String countryId;
+
+    @NotNull
+    private String capital;
+
+    @Pattern(regexp = "^[A-Z]{3}$", message = "Invalid currency code format")
+    private String currencyCode;
+
+    private String fipsCode;
+
+    @NotNull
+    @Pattern(regexp = "^[A-Z]{2}$", message = "Invalid country code format")
+    private String countryIso2;
+
+    @NotNull
+    @Pattern(regexp = "^[A-Z]{3}$", message = "Invalid ISO 3 code format")
+    private String countryIso3;
+
+    @NotNull
+    private String continent;
+
+    @NotNull
+    private String countryName;
+
+    @NotNull
+    private String currencyName;
+
+    @NotNull
+    private String countryIsoNumeric;
+
+    private String phonePrefix;
+
+    @NotNull
+    @Positive
+    private Long population;
+
+}
---
+++ src/main/java/com/vedasole/flight_service/repository/CountryRepo.java
@@ -0,0 +1,8 @@
+package com.vedasole.flight_service.repository;
+
+import com.vedasole.flight_service.entity.Country;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CountryRepo extends MongoRepository<Country, String> { }
