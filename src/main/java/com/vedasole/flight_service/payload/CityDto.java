36e72df18105bb89495437094ee62dfb3773f401
vedasole <vedasole001@gmail.com>
2024-10-03 02:45:59 +0530

    feat(city): add createdDate and updatedDate fields in City entity and CityDto

--- src/main/java/com/vedasole/flight_service/payload/CityDto.java
+++ src/main/java/com/vedasole/flight_service/payload/CityDto.java
@@ -1,4 +1,4 @@
-package com.vedasole.flight_service.entity;
+package com.vedasole.flight_service.payload;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
@@ -6,51 +6,39 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
-import org.springframework.data.mongodb.core.index.Indexed;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@EnableMongoAuditing
-@Document(collection = "cities")
-public class City {
+public class CityDto {
 
-    @Id
     private Long cityId;
 
     private String gmt;
 
-    @Indexed(unique = true)
     @NotNull
     @Size(min = 3, max = 3, message = "IATA code must be exactly 3 characters long")
     private String iataCode;
 
-    @Indexed
     @NotNull
     @Pattern(regexp = "^[A-Z]{2}$", message = "Invalid country code format")
     private String countryIso2;
 
-    @Indexed
     @NotNull
     private String cityName;
 
     private String geonameId;
 
+    @NotNull
     private Double latitude;
 
+    @NotNull
     private Double longitude;
 
-    @CreatedDate
     private LocalDateTime createdDate;
 
-    @LastModifiedDate
     private LocalDateTime updatedDate;
 
 }
