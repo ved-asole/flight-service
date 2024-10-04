3b725f5ca5067c3b945af8bd6415ce4bcd94f7db
vedasole <vedasole001@gmail.com>
2024-10-05 03:53:54 +0530

    refactor(exception): refactor ResourceNotFoundException message string

--- src/main/java/com/vedasole/flight_service/exception/ResourceNotFoundException.java
+++ src/main/java/com/vedasole/flight_service/exception/ResourceNotFoundException.java
@@ -13,7 +13,7 @@ public class ResourceNotFoundException extends RuntimeException {
                                      String fieldName,
                                      String fieldValue) {
 
-        super(String.format("%s not found with %s : %s",
+        super(String.format("%s not found with %s: %s",
                 resourceName, fieldName, fieldValue));
 
         this.resourceName = resourceName;
