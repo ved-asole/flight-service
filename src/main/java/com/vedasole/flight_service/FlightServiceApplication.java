f1f2f69325a1183a339e89c35f80b64c2fd7d8de
vedasole <vedasole001@gmail.com>
2024-10-06 23:19:23 +0530

    feat(main): update Page Serialization mode to VIA_DTO with SpringDataWebSupport

--- src/main/java/com/vedasole/flight_service/FlightServiceApplication.java
+++ src/main/java/com/vedasole/flight_service/FlightServiceApplication.java
@@ -8,10 +8,14 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.env.Environment;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
 @EnableMongoRepositories
 @Slf4j
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class FlightServiceApplication {
 
 	@Value("${spring.application.name}")
@@ -24,16 +28,16 @@ public class FlightServiceApplication {
 
 	public FlightServiceApplication(Environment environment) {
 		this.environment = environment;
-	}
-
-	@EventListener(ApplicationReadyEvent.class)
-	public void onApplicationReady() {
-        log.info("FlightServiceApplication started \uD83D\uDE80");
-		log.info("Project name: {}, Application name: {},  Port:{}", projectName, appName, environment.getProperty("local.server.port"));
     }
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlightServiceApplication.class, args);
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void onApplicationReady() {
+		log.info("FlightServiceApplication started \uD83D\uDE80");
+		log.info("Project name: {}, Application name: {}, Port:{}", projectName, appName, environment.getProperty("local.server.port"));
 	}
 
 }
