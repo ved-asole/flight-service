b741766e327c12f5e58de2d8cd575f838f09905c
vedasole <vedasole001@gmail.com>
2024-10-09 21:12:48 +0530

    feat(country): add existsByCountryId method in CountryRepo to ensure no duplicate countryIds

--- src/main/java/com/vedasole/flight_service/repository/CountryRepo.java
+++ src/main/java/com/vedasole/flight_service/repository/CountryRepo.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CountryRepo extends MongoRepository<Country, String> {
 
+    boolean existsByCountryId(String countryId);
+
     boolean existsByCountryName(String countryName);
 
     boolean existsByCountryIso2(String countryIso2);
