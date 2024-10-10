70522c06c9b250ceb6cc4cca3b9f3d3286571fab
vedasole <vedasole001@gmail.com>
2024-10-10 23:13:21 +0530

    feat(config): add AuditorAware bean for MongoAuditing

--- src/main/java/com/vedasole/flight_service/config/ApplicationConfig.java
+++ src/main/java/com/vedasole/flight_service/config/ApplicationConfig.java
@@ -3,7 +3,10 @@ package com.vedasole.flight_service.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+import java.util.Optional;
 
 @Configuration
 @EnableMongoAuditing
@@ -12,6 +15,11 @@ public class ApplicationConfig {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        return () -> Optional.of("crimsonSky-user");
     }
 
 }
