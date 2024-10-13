4d070a8c6ceb3a7e475518a2046394f919755412
vedasole <vedasole001@gmail.com>
2024-10-13 23:20:32 +0530

    feat(city): add checks and for unique iataCode existing CountryISO2 and no duplicates while adding and updating cities

--- src/main/java/com/vedasole/flight_service/service/service_impl/CityServiceImpl.java
+++ src/main/java/com/vedasole/flight_service/service/service_impl/CityServiceImpl.java
@@ -4,9 +4,12 @@ import com.vedasole.flight_service.entity.City;
 import com.vedasole.flight_service.exception.ResourceNotFoundException;
 import com.vedasole.flight_service.payload.CityDto;
 import com.vedasole.flight_service.repository.CityRepo;
+import com.vedasole.flight_service.repository.CountryRepo;
 import com.vedasole.flight_service.service.service_interface.CityService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
@@ -15,32 +18,44 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class CityServiceImpl implements CityService {
 
     private final CityRepo cityRepo;
+    private final CountryRepo countryRepo;
     private final ModelMapper modelMapper;
     private static final String CITY_STRING = "City";
 
-    @Transactional
     @Override
+    @Transactional
     public CityDto addCity(CityDto cityDto) {
+        if (cityRepo.existsByIataCode(cityDto.getIataCode()))
+            throw new ResourceNotFoundException(CITY_STRING, "IATA code", cityDto.getIataCode());
+        if (!countryRepo.existsByCountryIso2(cityDto.getCountryIso2()))
+            throw new DataIntegrityViolationException("Country with ISO2 does not exist: " + cityDto.getCountryIso2());
         return convertToDto(cityRepo.save(convertToEntity(cityDto)));
     }
 
-    @Transactional
     @Override
+    @Transactional
     public CityDto updateCity(String cityId, CityDto cityDto) {
         City updatedCity = cityRepo.findById(cityId).map(city -> {
 //            To prevent updating createdDate
+            cityDto.setCityId(cityId);
             cityDto.setCreatedDate(city.getCreatedDate());
             modelMapper.map(cityDto, city);
+            // Check if the new IATA code already exists in another city
+            if (cityRepo.existsByIataCodeAndCityIdNot(city.getIataCode(), cityId))
+                throw new ResourceNotFoundException(CITY_STRING, "IATA code", city.getIataCode());
+            if (countryRepo.existsByCountryIso2(city.getCountryIso2()))
+                throw new DataIntegrityViolationException("Country with ISO2 code already exists: " + cityDto.getCountryIso2());
             return cityRepo.save(city);
         }).orElseThrow(() -> new ResourceNotFoundException(CITY_STRING, "id", cityId));
         return convertToDto(updatedCity);
     }
 
-    @Transactional
     @Override
+    @Transactional
     public CityDto deleteCity(String cityId) {
         return cityRepo.findById(cityId).map(city -> {
             cityRepo.delete(city);
@@ -48,25 +63,27 @@ public class CityServiceImpl implements CityService {
         }).orElseThrow(() -> new ResourceNotFoundException(CITY_STRING, "id", cityId));
     }
 
-    @Transactional(readOnly = true)
     @Override
+    @Transactional(readOnly = true)
     public CityDto getCityById(String cityId) {
         return cityRepo.findById(cityId).map(this::convertToDto)
                 .orElseThrow(() -> new ResourceNotFoundException(CITY_STRING, "id", cityId));
     }
 
-    @Transactional(readOnly = true)
     @Override
+    @Transactional(readOnly = true)
     public Page<CityDto> getAllCities(int page, int size, String sortBy, String order) {
         return cityRepo.findAll(PageRequest.of(page, size, Sort.by(Sort.Direction.fromString(order), sortBy)))
                 .map(this::convertToDto);
     }
 
     public CityDto convertToDto(City city) {
+        if (city == null) return null;
         return modelMapper.map(city, CityDto.class);
     }
 
     public City convertToEntity(CityDto cityDto) {
+        if (cityDto == null) return null;
         return modelMapper.map(cityDto, City.class);
     }
 
