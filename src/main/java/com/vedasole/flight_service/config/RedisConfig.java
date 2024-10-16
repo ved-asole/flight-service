9742f544400170b6dceef356aee9453ca775fdd1
vedasole <vedasole001@gmail.com>
2024-10-16 21:25:16 +0530

    refactor(cache): refactor ReactConfig to serialize values in string format

--- src/main/java/com/vedasole/flight_service/config/RedisConfig.java
+++ src/main/java/com/vedasole/flight_service/config/RedisConfig.java
@@ -14,6 +14,7 @@ public class RedisConfig {
         RedisTemplate<String, String> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
         template.setHashKeySerializer(new StringRedisSerializer());
         return template;
     }
