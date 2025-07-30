e85a775ae065d59911aad2a83abc6384f03b94ae
vedasole <vedasole001@gmail.com>
2025-07-30 22:14:19 +0530

    refactor(Airport): remove unnecessary validation annotations from Airport entity fields

--- src/main/java/com/vedasole/flight_service/entity/Airport.java
+++ src/main/java/com/vedasole/flight_service/entity/Airport.java
@@ -1,9 +1,6 @@
 package com.vedasole.flight_service.entity;
 
-import jakarta.validation.constraints.Digits;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -27,37 +24,28 @@ public class Airport {
     private String airportId;
 
     @Indexed
-    @NotNull
     private String airportName;
 
     @Indexed(unique = true)
-    @Size(min = 3, max = 3)
     private String iataCode;
 
     @Indexed
-    @Size(min = 4, max = 4)
     private String icaoCode;
 
     @Indexed
     private String cityIataCode;
 
     @Indexed
-    @Pattern(regexp = "^[A-Z]{2}$", message = "Invalid country code")
     private String countryIso2;
 
     private String countryName;
 
-    @Digits(integer = 10, fraction = 0)
     private Long geonameId;
 
-    @NotNull
-    @Digits(integer = 2, fraction = 2)
     private Float gmt;
 
-    @NotNull
     private Double latitude;
 
-    @NotNull
     private Double longitude;
 
     @Pattern(regexp = "^[A-Za-z_]+/[A-Za-z_]+$", message = "Invalid timezone format")
